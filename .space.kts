7c4ec58670138bb5a402557a58f157b0b1309333
Amit Khetan <amit.khetan.70@50gramx.io>
2023-08-08 12:36:23 +0000

    add parameters

--- .space.kts
+++ .space.kts
@@ -5,13 +5,18 @@ flowchart TD
     C -->|Done| D(Configure Domain Directories)
     D -->|Done| E(Build Python Domain)
     E -->|Done| F(Push Python Domain)
-    F -->|Done| G[End]
+    F -->|Done| G(Ensure Hosted Python Index)
+    G -->|Done| H(Build & Publish Python Package)
+    H -->|Done| I[End]
     A -->|Nodejs Domain Build| N(Checkout Service Core)
-    N -->|Done| H(Checkout Nodejs Domain)
-    H -->|Done| I(Configure Domain Directories)
-    I -->|Done| J(Build Nodejs Domain)
-    J -->|Done| K(Push Nodejs Domain)
-    K -->|Done| L[End]
+    N -->|Done| O(Checkout Nodejs Domain)
+    O -->|Done| P(Configure Domain Directories)
+    P -->|Done| Q(Build Nodejs Domain)
+    Q -->|Done| R(Push Nodejs Domain)
+    R -->|Done| S(Ensure Hosted Nodejs Index)
+    S -->|Done| T(Build & Publish NPM Package)
+    T -->|Done| I[End]
+
 */
 
 job("Distribute Core Domain Packages") {
@@ -24,6 +29,21 @@ job("Distribute Core Domain Packages") {
             }
         }
     }
+
+    // check out eapp-python-domain to /mnt/space/work/eapp-python-domain
+    git("eapp-python-domain")
+
+	parameters {
+      text("EAPP_PROTO_SRC_DIR", value = "/mnt/space/work/eapp-service-core/src/main/proto")
+      text("EAPP_PROTO_PYTHON_OUT_DIR", value = "/mnt/space/work/eapp-python-domain/src/eapp_python_domain")
+      text("EAPP_PROTO_NODEJS_OUT_DIR", value = "/mnt/space/work/eapp-nodejs-domain/eapp-nodejs-domain")
+      text("PROTO_INCLUDES", value = "")
+      text(
+        "PROTO_INCLUDE_DIRS",
+        value = """${EAPP_PROTO_SRC_DIR}/google/api/*.proto,
+        	"""
+      )
+    }
     
 	parallel {
 
@@ -33,6 +53,13 @@ job("Distribute Core Domain Packages") {
                   content = """
                     pwd
                     ls -l
+                    env
+                    env.PROTO_INCLUDE_DIRS.tokenize(',\n').each {
+                        env.TEMP = "${it}"
+                        env.TEMP = env.TEMP.trim()
+                        env.PROTO_INCLUDES = "${PROTO_INCLUDES} ${TEMP}"
+                    }
+
                   """
                 }
             }
