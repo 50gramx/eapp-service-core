0b52cd3aaff4a1f01b92645514f848034892b8f0
Amit Khetan <amit.khetan.70@50gramx.io>
2023-08-25 03:59:00 +0000

    fix proto includes

--- .space.kts
+++ .space.kts
@@ -51,10 +51,7 @@ job("Distribute Core Domain Packages") {
             
             val PROTO_INCLUDE_DIRS = """
                 ${EAPP_PROTO_SRC_DIR}/google/api/*.proto,
-                ${EAPP_PROTO_SRC_DIR}/gramx/fifty/zero/ethos/identity/multiverse/core/entity/epme_1005/*.proto,
-                ${EAPP_PROTO_SRC_DIR}/gramx/fifty/zero/ethos/identity/multiverse/core/entity/epme_1005/capability/*.proto,
-                ${EAPP_PROTO_SRC_DIR}/gramx/seventy/zero/ethos/gramxpro/multiverse/core/entity/epe_1001/*.proto,
-                ${EAPP_PROTO_SRC_DIR}/gramx/seventy/zero/ethos/gramxpro/multiverse/core/entity/epe_1002/*.proto
+                ${EAPP_PROTO_SRC_DIR}/ethos/elint/entities/*.proto,
             """.trimIndent()
             
             var PROTO_INCLUDES = ""
