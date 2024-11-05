ad80129ccefd9dba56004e7afac98c458ab20d53
khetana <amit.khetan.70@50gramx.io>
2024-11-05 18:34:11 +0530

    fixed dart publish

--- .space.kts
+++ .space.kts
@@ -489,7 +489,7 @@ job("Distribute Core Domain Packages") {
                     dart pub token list
                     dart pub token add https://dart.pkg.jetbrains.space/50gramx/p/main/dart-delivery --env-var JB_SPACE_CLIENT_TOKEN
                     dart pub token list
-                    dart pub publish --skip-validation
+                    dart pub publish --force --skip-validation
                     echo "--------------------------------------------------------------------"
                   """
                 }
